63d4527c69311eb86e8fce937b668a5957fd0697
Zoe0426 <90545407+Zoe0426@users.noreply.github.com>
2023-10-26 19:23:03 +0800

    og:image

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
+        <meta property='og:image' content='https://zoe0426.github.io/bros/bg1.jpeg' />
         <link rel='icon' href='1697092782.ico' />
       </Head>
       <body>
