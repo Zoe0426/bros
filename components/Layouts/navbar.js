fd8d521bfee2441b328f85a3d584e960f1fd5d5d
Zoe0426 <90545407+Zoe0426@users.noreply.github.com>
2023-10-26 16:10:59 +0800

    test

--- components/Layouts/navbar.js
+++ components/Layouts/navbar.js
@@ -96,7 +96,7 @@ export default function Navbar({ type = "" }) {
             </button>
             {/* <Image src='/logo.png' alt='' className='logo' width={180} height={86} /> */}
             <Link href='/'>
-              <img src="/public/logo.png" alt='' />
+              <img src="./public/logo.png" alt='' />
             </Link>
           </div>
           <div className={Styles.links}>
